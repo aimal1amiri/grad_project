5d2143699e34ce7259bafa63dacc3e74ad8d0f21
emir2269 <ali724345@gmail.com>
2024-10-01 16:30:51 +0300

    creating methods to fetch movies from TMDB website

--- Back-end/start_back-end.js
+++ Back-end/start_back-end.js
@@ -23,3 +23,16 @@ web.listen(PORT,()=>{
     console.log("back-end server has started on "+PORT);
     connectDB();
 });
+
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer '+ENV_VARS.TMDB_API_KEY
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
