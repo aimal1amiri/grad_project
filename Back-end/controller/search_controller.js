a06206d656b74d971c91c4f083e8d507630632af
emir2269 <ali724345@gmail.com>
2024-11-27 23:58:15 +0300

    created end point for searchTvShow

--- Back-end/controller/search_controller.js
+++ Back-end/controller/search_controller.js
@@ -66,4 +66,33 @@ export async function searchMovie(req,res){
 }
 export async function searchTvShow(req,res){
     //https://api.themoviedb.org/3/search/tv?include_adult=false&language=en-US&page=1
+
+    const {query} = req.params;
+
+    try {
+        const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=en-US&page=1`)
+
+        if(response.results.length === 0){
+            return res.status(404).send(null)
+        }
+
+        await User.findByIdAndUpdate(req.params._id,{
+            searchHistory:{
+                $push:{
+                    id:response.results[0].id,
+                    title:response.results[0].name,
+                    image:response.results[0].poster_path,
+                    createdAt:new Date(),
+                    searchType:"Tv Show",
+                },
+            },
+        
+        });
+
+        res.status(200).json({success:true, content:response.results})
+    } catch (error) {
+        console.log("error in search Tv show: ",error.message);
+        res.status(500).json({success:false, message:"Internal error"})
+        
+    }
 }
