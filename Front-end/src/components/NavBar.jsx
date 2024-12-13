1c0d2bdaac2e247716d253da116796ef907e9e31
emir2269 <ali724345@gmail.com>
2024-12-14 00:12:54 +0300

    some bugs have been fixed

--- Front-end/src/components/NavBar.jsx
+++ Front-end/src/components/NavBar.jsx
@@ -1,10 +1,14 @@
 import { set } from 'mongoose';
 import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
+import {LogOut, Menu, Search} from 'lucide-react'
+import { useAuthGlobalState } from '../store/authUser';
 
 const NavBar = () => {
 
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const {user, logout} = useAuthGlobalState();
 
     const toggleMobileMenu = () => {
         setIsMobileMenuOpen(!setIsMobileMenuOpen)
@@ -16,11 +20,11 @@ const NavBar = () => {
         <div className='flex items-center gap-10 z-50 '>
 
             <Link to="/">
-            <img src='/netflix.logo.png' alt='Netflix Logo' className='w-32 sm:w-40'/>
+            <img src='/netflix-logo.png' alt='Netflix Logo' className='w-32 sm:w-40'/>
             </Link>
 
             {/*for desktop */}
-            <div className='hidden sm:flex gap-2 items-center'>
+            <div className='hidden sm:flex gap-4 items-center'>
                 <Link to='/' className='hover:underline'>
                 Movies
                 </Link>
@@ -36,6 +40,19 @@ const NavBar = () => {
             </div>
 
 
+        </div>
+
+        <div className='flex gap-2 items-center z-50'>
+            <Link to={"/search"}>
+            <Search className='size-6 cursor-pointer'/>
+            </Link>
+
+            <img src={user.image} alt='Avatar' className='h-8 rounded cursor-pointer'/>
+            <LogOut className='size-6 cursor-pointer ' onClick={logout} />
+
+            <div className='sm:hidden'>
+                <Menu className='size-6 cursor-pointer' onClick={toggleMobileMenu}/>
+            </div>
         </div>
 
         {isMobileMenuOpen && (
