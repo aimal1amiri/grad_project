f6883255a7030f2fdb8e295b8f27a741af365228
emir2269 <ali724345@gmail.com>
2024-12-31 00:35:58 +0300

    all fuctionalities for -streamingvideopage- has been setup .

--- Front-end/src/pages/StreamingVideoPage.jsx
+++ Front-end/src/pages/StreamingVideoPage.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { useMovieTvContentStore } from '../store/movie&TvContent';
 import axios from 'axios';
 import NavBar from '../components/NavBar';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import ReactPlayer from 'react-player'
+import { IMAGE_BASE_URL, ORIGINAL_IMAGE_BASE_URL } from '../utils/helperVariables';
+import WaitingPage from './WaitingPage';
 
 const StreamingVideoPage = () => {
   
@@ -13,10 +15,11 @@ const StreamingVideoPage = () => {
 
     const [trailers, setTrailers] = useState([])
     const [currentTrailerIndex, setCurrentTrailerIndex] = useState(0);
-    const [loadingSpinner, setLoadingSpinner] = useState(true);
+    const [loadingPage, setLoadingPage] = useState(true);
     const [content, setContent]= useState({});
     const [similarContent, setSimilarContent] = useState([]);
     const {contentType} =useMovieTvContentStore();
+    const sliderRef= useState(null);
 
 
     useEffect(() => {
@@ -63,7 +66,7 @@ const StreamingVideoPage = () => {
                 const response = await axios.get(`/api/v1/${contentType}/${id}/details`);
                 setContent(response.data.message);
 
-                console.log("inside: ", response)
+                //console.log("inside: ", response)
 
             } catch (error) {
                 if(error.message.includes('404')){
@@ -71,7 +74,7 @@ const StreamingVideoPage = () => {
                 }
                 
             }finally{
-                setLoadingSpinner(false)
+                setLoadingPage(false)
             }
         }
         getMovieAndTvShowDetails();
@@ -92,6 +95,36 @@ const StreamingVideoPage = () => {
         }
     }
 
+
+    const scrollRight=() =>{
+        if(sliderRef.current){
+            sliderRef.current.scrollBy({left: sliderRef.current.offsetWidth, behavior:"smooth"})
+        }
+    }
+
+    const scrollLeft=() =>{
+        if(sliderRef.current){
+            sliderRef.current.scrollBy({left: -sliderRef.current.offsetWidth, behavior : "smooth"});
+        }
+    }
+
+
+    if(loadingPage){
+        return <div className='min-h-screen bg-black p-10'> <WaitingPage/></div>
+    }
+
+    if(!content){
+        return (
+            <div className='bg-black text-white h-screen'>
+                <div className='max-w-6xl mx-auto'>
+                    <NavBar />
+                    <div className='text-center mx-auto px-4 py-8 h-full mt-40'>
+                        <h2 className='text-2xl sm:text-5xl font-bold text-balance'>Not Avaliable</h2>
+                    </div>
+                </div>
+            </div>
+        )
+    }
     
 
   return (
@@ -133,9 +166,38 @@ const StreamingVideoPage = () => {
                     <p className='mt-2 text-lg'>
                         {(content?.release_date || content?.first_air_date)} | {" "} {content?.adult ? ( <span className='text-red-600'>18+</span>) : (<span className='text-green-500'>PG-13</span>)} {" "}
                     </p>
+
+                    <p className='mt-4 text-lg'> {content?.overview}</p>
                 </div>
+                <img src={ORIGINAL_IMAGE_BASE_URL + content?.poster_path}
+                alt='Poster image'
+                className='max-h-[600px] rounded-md'/>
 
             </div>
+
+            {similarContent.length > 0 && (
+                <div className='mt-12 max-w-5xl mx-auto relative'>
+                    <h3 className='text-3xl font-bold mb-4'>
+                        Similar {content?.title || content?.name}
+                    </h3>
+                    <div className='flex overflow-x-scroll scrollbar-hide gap-4 pb-4 group' ref={sliderRef}>
+                        {similarContent.map((content) => {
+                            if(content.poster_path === null){
+                                return null
+                            }
+                            return (
+                                <Link key={content.id} to={`/watch/${content.id}`} className='w-52 flex-none'> 
+                                <img src={IMAGE_BASE_URL+content.poster_path} alt='image path' className='w-full h-auto rounded-md'/> 
+                                <h4 className='mt-2 text-lg font-semibold'>{content?.title || content?.name}</h4>
+                                </Link>
+                            )
+                        })}
+
+                        <ChevronRight className='absolute top-1/2 -translate-y-1/2 right-2 w-8 h-8 opacity-0 group-hover:opacity-100 transition-all duration-300 cursor-pointer bg-orange-600 text-white rounded-full' onClick={scrollRight} />
+                        <ChevronLeft className='absolute top-1/2 -translate-y-1/2 left-2 w-8 h-8 opacity-0 group-hover:opacity-100 transition-all duration-300 cursor-pointer bg-orange-600 text-white rounded-full' onClick={scrollLeft} />
+                    </div>
+                </div>
+            )}
 
         </div>
     </div>
